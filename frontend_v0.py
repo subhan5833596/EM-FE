68dca2c26393d8ba4af6d22afad6739bacbd6c23
subhan5833596 <135039914+subhan5833596@users.noreply.github.com>
2024-08-12 14:23:19 +0500

    Update frontend_v0.py

--- frontend_v0.py
+++ frontend_v0.py
@@ -45,7 +45,7 @@ def registration():
               # re-prompting the user for permission. Recommended for web server apps.
               access_type='offline',
               # Enable incremental authorization. Recommended as a best practice.
-              include_granted_scopes='true')
+              include_granted_scopes='true',prompt='consent')
         
           # Store the state so the callback can verify the auth server response.
         flask.session['state'] = state

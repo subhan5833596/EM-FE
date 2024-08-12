04236c1d43420cae7e39bf73e9a98e66ac0e6dc1
subhan5833596 <135039914+subhan5833596@users.noreply.github.com>
2024-08-12 14:37:25 +0500

    Update frontend_v0.py

--- frontend_v0.py
+++ frontend_v0.py
@@ -112,7 +112,7 @@ def oauth2callback():
 
 
 def credentials_to_dict(credentials):
-  expiry_time = datetime.utcnow() + datetime.timedelta(hours=1)
+  expiry_time = datetime.now() + datetime.timedelta(hours=1)
   expiry = expiry_time.isoformat() + 'Z'  # Format it as an ISO 8601 string
   return {'token': credentials.token,
           'refresh_token': credentials.refresh_token,
